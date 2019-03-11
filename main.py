81ac10fda50849616079ea6db2f3b85fd096ef99
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-03-11 18:33:22 +0300

    Added file common_data.py for TTBS class.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from datetime import datetime
 import pytz
 import os
 
+# See common_data.py to understant how it works.
+from common_data import *
 
 # If there's no 'tokens' directory.
 if not os.path.exists('tokens/'):
@@ -23,26 +25,7 @@ global old_ttb
 
 ############################# Timetables #########################################
 
-# Class for storing timetable's options.
-class TTBS():
-    pass
 
-pravo_c1 = TTBS()
-pravo_c2 = TTBS()
-pravo_c3 = TTBS()
-pravo_c4 = TTBS()
-
-pravo_c1.url = 'https://law.bsu.by/pub/2/Raspisanie_1_pravo.xls'
-pravo_c1.name = 'Правоведение, 1-й курс'
-
-pravo_c2.url = 'https://law.bsu.by/pub/2/Raspisanie_2_pravo.xls'
-pravo_c2.name = 'Правоведение, 2-й курс'
-
-pravo_c3.url = 'https://law.bsu.by/pub/2/Raspisanie_3_pravo.xls'
-pravo_c3.name = 'Правоведение, 3-й курс'
-
-pravo_c4.url = 'https://law.bsu.by/pub/2/Raspisanie_4_pravo.xls'
-pravo_c4.name = 'Правоведение, 4-й курс'
 
 
 # Get timetable's mtime using urllib module. 
--- ttb-checker.py
+++ ttb-checker.py
@@ -7,10 +7,21 @@ from datetime import datetime
 import os
 import time
 
+# See common_data.py to understant how it works.
+from common_data import *
+
 # Use dev token
 token_to_use = 'token.dev'
 #token_to_use = 'token.release'
 
+
+
+# If there's no 'tokens' directory.
+if not os.path.exists('tokens/'):
+    print("You should create 'tokens/' dir and put 'token.dev' or 'token.release' file there. Exit")
+    exit()
+
+# If there is no token file.
 try:
     token_file = open('tokens/' + token_to_use) 
 except Exception:
