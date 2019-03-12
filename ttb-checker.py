040dcbe8ddacf20289ba7a1ec561f6be6fb378bb
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-03-12 20:07:17 +0300

    Fixed '010' error in converting date to gmt+3.

--- ttb-checker.py
+++ ttb-checker.py
@@ -35,8 +35,42 @@ token_file.close()
 
 bot = telegram.Bot(token=token_str)
 
+####################################################
+
+
+# Checks ttb' update time.
+def ttb_gettime(ttb):
+    
+    # Send HTTP response.
+    response =  urllib.request.urlopen(ttb.url)
+    
+    # Get date and time from HTTP header.
+    native_date = ' '.join(dict(response.headers)['Last-Modified'].rsplit()[1:-1])
+    
+    # Transfer date to datetime format.
+    date = list(str(datetime.strptime(native_date, '%d %b %Y %H:%M:%S')))
+    print(date)
+    
+    # Transfer to GMT+3. Prevent "010:00, 011:00" if hour is less than 10.
+    hour = str(int(str(date[11]) + str(date[12])) + 3)
+    
+    if len(hour) == 1:
+        hour = '0' + hour
+
+    date[11] = hour[0]
+    date[12] = hour[1]
+    
+    date = ''.join(date)
+    
+    return(date)
+
+
+
 while True:
-	
-	
-	
-	time.sleep(60)
+    
+    for ttb in [pravo_c1, pravo_c2, pravo_c3, pravo_c4]:
+        print(ttb.name, ttb_gettime(ttb))
+        
+    
+    
+    time.sleep(60)
