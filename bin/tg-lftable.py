785436789cb2a4607aae41221e86b3a78afb2bc8
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-05-05 17:05:53 +0300

    A lot of small changes like fixing comments, deleting extra lines and etc.

--- bin/tg-lftable.py
+++ bin/tg-lftable.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-# Add directory with local modules to path
+# Add src/' directory with local modules to path
 sys.path.append('src')
 
 from telegram.ext import Updater
@@ -10,38 +10,23 @@ from telegram.ext import CommandHandler, CallbackQueryHandler, JobQueue
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import ParseMode
 
-import urllib.request
 from datetime import datetime
-
-import pytz
 
 import logging
 import sqlite3
 
 import time
 
-import ssl
-
-# Import all local modules
+# Import all local modules, see 'src/' directory to understand how everythong works
 from static import *
 from messages import *
 from backend import *
 from keyboards import *
 from logger import *
 
-######################## Logging settings ##############################
-
-# Nothing will work without logging
-if not os.path.exists('log/'):
-    try:
-        os.mkdir('log/')
-    except Exception:
-        print("CRITICAL ERROR: can't create 'log/' directory. Exit")
-        sys.exit()
-
-
+######################## Exceptions logger #############################
 # Uncomment this and see 'log/lftable-exceptions.log' if something goes wrong.
-#"""
+"""
 # Logger for all exceptions.
 logging.basicConfig(filename=log_dir + "lftable-exceptions.log", level=logging.DEBUG)
 exception_logger = logging.getLogger('exception_logger')
@@ -50,58 +35,52 @@ exception_logger = logging.getLogger('exception_logger')
 def my_handler(type, value, tb):
     exception_logger.exception("Uncaught exception: {0}".format(str(value)))
 sys.excepthook = my_handler
-#"""
+"""
+########################################################################
 
-
-
-# Write 'program started' message to log
-logger.info("the program was STARTED now")
-
-
-
-
-############################### Bot commands ############################################
 
 # /start command --> calls main menu.
 def start(bot, update):
-    update.message.reply_text(main_menu_message(), parse_mode=ParseMode.HTML,
-                           reply_markup=main_menu_keyboard(), timeout=25)
+    update.message.reply_text(main_menu_message(),
+                              parse_mode=ParseMode.HTML,
+                              reply_markup=main_menu_keyboard(),
+                              timeout=25)
 
-   
+
 # Bot's behavior depending on the button pressed.
 def button_actions(bot, update):
 
-
     query = update.callback_query
-    
+
     # To know which button was pressed.
     callback = query.data
     # The user who pressed the button
     user_id = query.message.chat_id
-    
+
     # If a new user joins the bot, this function writes his id to the 'statistics.db'
     send_statistics(user_id)
-    
+
     # Write to log
     logger.debug('user ' + str(user_id) + " pressed button '" + callback + "'")
-    
+
 
     # Sends main menu.
     if callback == 'main_menu':
 
         bot.edit_message_text(chat_id = query.message.chat_id,
-                                message_id = query.message.message_id,
-                                text=main_menu_message(),
-                                # Used for bold font
-                                parse_mode=ParseMode.HTML,
-                                reply_markup=main_menu_keyboard(), timeout=25)
-                                
-    
+                              message_id = query.message.message_id,
+                              text=main_menu_message(),
+                              # Used for bold font
+                              parse_mode=ParseMode.HTML,
+                              reply_markup=main_menu_keyboard(), timeout=25)
+
+
     # Sends message with certain timetable info depending on the button pressed before.
-    if callback in  ['answer_p1', 'answer_p2', 'answer_p3', 'answer_p4', 
+    if callback in  ['answer_p1', 'answer_p2', 'answer_p3', 'answer_p4',
                              'answer_m1', 'answer_m2',
                              'refresh', 'notify']:
-        
+
+        # If any of TTB buttons is pressed
         if callback == 'answer_p1':
             current_ttb = pravo_c1
         elif callback == 'answer_p2':
@@ -110,12 +89,13 @@ def button_actions(bot, update):
             current_ttb = pravo_c3
         elif callback == 'answer_p4':
             current_ttb = pravo_c4
-    
         elif callback == 'answer_m1':
             current_ttb = mag_c1
         elif callback == 'answer_m2':
             current_ttb = mag_c2
-        
+
+
+        # If 'refresh' or 'notify' button is pressed
         elif callback in ['refresh', 'notify']:
 
             # Detect the timetable checking first line of the ttb message
@@ -125,41 +105,41 @@ def button_actions(bot, update):
                 if message.text.split('\n')[0] == ttb.name:
                     current_ttb = ttb
 
-            
+
             if callback == 'notify':
 
-                # Disable if user id is already in the dbs. Delete row from db.
+                # Disable if user id is already in the db. Delete row from db.
                 if check_user_notified(current_ttb, user_id):
                     conn = sqlite3.connect(notifications_db)
                     cursor = conn.cursor()
-        
+
                     cursor.execute('DELETE FROM ' + current_ttb.shortname + ' WHERE (users = \'' + str(user_id) + '\')')
                     result = cursor.fetchall()
-            
+
                     # Save changes and close.
                     conn.commit()
                     conn.close()
-            
+
                     # Write to log
                     logger.info('user ' + str(user_id) + " disabled notifications for the '" + current_ttb.shortname + "' timetable")
-                    
-                # Enable notifying. Insert user id into db.
+
+                # Enable notifications. Insert user id into the db.
                 else:
                     conn = sqlite3.connect(notifications_db)
                     cursor = conn.cursor()
-        
+
                     cursor.execute('INSERT INTO ' + current_ttb.shortname + ' VALUES (\'' + str(user_id) + '\')')
                     result = cursor.fetchall()
-            
+
                     # Save changesa and close.
                     conn.commit()
                     conn.close()
-                
+
                     # Write to log
                     logger.info('user ' + str(user_id) + " enabled notifications for the '" + current_ttb.shortname + "' timetable")
 
-        
-        
+
+
         # Edit message depending on the callback
         bot.edit_message_text(chat_id=query.message.chat_id,
                         message_id=query.message.message_id,
@@ -168,149 +148,151 @@ def button_actions(bot, update):
                         parse_mode=ParseMode.HTML,
                         reply_markup=answer_keyboard(current_ttb, user_id), timeout=25)
 
-    
-    # Deletes notification message.
+
+    # Deletes notification message if 'delete' button is pressed.
     if callback == 'delete_notification':
         bot.delete_message(user_id, query.message.message_id)
         # Write to log
         logger.info('user ' + str(user_id) + " deleted notification (message: " + str(query.message.message_id) + ")")
-    
 
 
-# Notification message. 
+
+# A timejob for notifications
 def notifications_timejob(bot, job):
-	
+
+    # Connect to the times.db
     conn_times_db = sqlite3.connect(times_db)
     cursor_times_db = conn_times_db.cursor()
-    
+
+    # See 'all_timetables' list in 'src/static.py'
     for checking_ttb in all_timetables:
-        
+
         # Get ttb update time from law.bsu.by
         update_time = ttb_gettime(checking_ttb).strftime('%d.%m.%Y %H:%M:%S')
-        
-        
+
         # Get old update time from db.
         cursor_times_db.execute("SELECT time  FROM times WHERE (ttb = ?)", (checking_ttb.shortname,));
         result = cursor_times_db.fetchall()
         old_update_time = result[0][0]
         del(result)
-        
-        
-        # String dates to datetime objects
+
+        # Convert string dates to datetime objects
         dt_update_time = datetime.strptime(update_time, '%d.%m.%Y %H:%M:%S')
         dt_old_update_time = datetime.strptime(old_update_time, '%d.%m.%Y %H:%M:%S')
-        
-        
-        # If the timetable was updated, sends it to all users 
+
+
+        # Compare the two dates
+        # If the timetable was updated, sends it to all users
         #+ from certain table in 'users.db'
         if dt_update_time > dt_old_update_time:
-            
+
             # Write to log
             logger.info("'" + checking_ttb.shortname + "' timetable was updated at " + update_time)
 
-            
             # Connect to users db.
             conn_notifications_db = sqlite3.connect(notifications_db)
             cursor_notifications_db = conn_notifications_db.cursor()
-        
+
             cursor_notifications_db.execute('SELECT users FROM ' + checking_ttb.shortname)
             result = cursor_notifications_db.fetchall()
 
             conn_notifications_db.close()
-    
+
             # List for users notifed about current timetable updates.
             users_to_notify = []
             for i in result:
                 users_to_notify.append(i[0])
             del(result)
-            
-            
-            
-            # Send notifications to users.
+
+
+            # Send a notification to each user.
             for user_id in users_to_notify:
                 bot.send_message(chat_id=user_id, text=notification_message(checking_ttb, dt_update_time), reply_markup=notify_keyboard(), parse_mode=ParseMode.HTML)
-                
+
                 # Write to log
                 logger.info("'" + checking_ttb.shortname + "' notification was sent to user " + str(user_id))
-                
+
+                # A delay to prevent any spam control exceptions
                 time.sleep(send_message_interval)
-                
-            
-            # Writing new update time to the database.
+
+
+            # Write new update time to the database.
             cursor_times_db.execute("UPDATE times SET time = '" + update_time + "' WHERE (ttb = ?)", (checking_ttb.shortname,));
             conn_times_db.commit()
-            
+
+
+        # A delay to prevent any spam control exceptions
         time.sleep(send_message_interval)
-    
+
     # Close 'times.db' until next check.
     conn_times_db.close()
-    
-    
 
 
-
-############################# Bot settings #############################
 
 def main():
-     
-    
+
+    # Write 'program started' message to log
+    logger.info("the program was STARTED now")
+
+    # Parse arguments
     if len(sys.argv) != 2:
         print("Invalid arguments passed. Use '-r' option to run with release token, '-d' - to run with development token")
         logger.critical("invalid arguments, exit")
         sys.exit()
-    
-    # See token.py
+
+
     if sys.argv[1] == "-r":
         print("Started in release mode")
         try:
+            # See src/tokens.py file
             from tokens import release_token
             token_str = release_token
         except Exception:
             logger.critical("no 'release_token' in tokens.py file, exit")
             print("no 'release_token' in token.py, exit")
-        
+
     elif sys.argv[1] == "-d":
         print("Started in development mode")
         try:
+            # See src/tokens.py file
             from tokens import dev_token
             token_str = dev_token
         except Exception:
             logger.critical("no 'dev_token' in tokens.py file, exit")
             print("no 'dev_token' in token.py, exit")
-   
+
     else:
         print("Invalid arguments passed. Use '-r' option to run with release token, '-d' - with development token")
         logger.critical("invalid arguments, exit")
         sys.exit()
-        
 
-    
-    # Create directories and files.
+
+
+    # Create directories and files if there if no
+    # See 'src/backend.py' file
     first_run_check()
-    # Write times to the db after the start 
-    # to prevent late notifications.
+    # Write times to the db after the start  to prevent late notifications.
     db_set_times_after_run()
-    
-    updater = Updater(token_str)    
+
+
+    updater = Updater(token_str)
     dp = updater.dispatcher
 
-    # Run ttb checks on on schedule (see check_updates_interval in 'static.py'
+    # Run timejob for notificatins
     job = updater.job_queue
     job.run_repeating(notifications_timejob, interval = check_updates_interval, first=0)
-    
-    
+
     # Handlers
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CallbackQueryHandler(button_actions))
-
 
     # Checking for updates.
     updater.start_polling(clean=True)
     # Stop bot if  <Ctrl + C> is pressed.
     updater.idle()
 
-    
-    
+
+
 if __name__ == "__main__":
     main()
 
--- src/keyboards.py
+++ src/keyboards.py
@@ -5,7 +5,7 @@ from backend import check_user_notified
 # Main menu keyboard.
 # 4 buttons in main menu. Each button is designed for the corresponding timetable (1-4 course)
 def main_menu_keyboard():
-    
+
     pravo_c1.btn = InlineKeyboardButton('Правоведение - 1⃣', callback_data='answer_p1')
     pravo_c2.btn = InlineKeyboardButton('Правоведение - 2⃣', callback_data='answer_p2')
     pravo_c3.btn = InlineKeyboardButton('Правоведение - 3⃣', callback_data='answer_p3')
@@ -13,45 +13,45 @@ def main_menu_keyboard():
 
     mag_c1.btn = InlineKeyboardButton('Магистратура - 1⃣', callback_data='answer_m1')
     mag_c2.btn = InlineKeyboardButton('Магистратура - 2⃣', callback_data='answer_m2')
-    
+
     keyboard = [[pravo_c1.btn, pravo_c2.btn],
                 [pravo_c3.btn, pravo_c4.btn],
-                [mag_c1.btn, mag_c2.btn]]           
-          
+                [mag_c1.btn, mag_c2.btn]]
+
     return(InlineKeyboardMarkup(keyboard))
 
 
 
 # Menu for specific timetable. One button returns to main menu, one refreshes date and time info.
 def answer_keyboard(ttb, user_id):
-    
+
     # Button to refresh current answer menu (so you don't have to come back to main menu).
     refresh_button = InlineKeyboardButton('🔄 Обновить страницу', callback_data='refresh')
-    
+
     # For notify function. Adds info to DB.
     if check_user_notified(ttb, user_id):
         notify_text = u'🔕 Не уведомлять'
     else:
         notify_text = u'🔔 Уведомлять'
-    
-    # Button to put user id into db in order to notify him when the timetable is updated. 
+
+    # Button to put user id into db in order to notify him when the timetable is updated.
     notify_button = InlineKeyboardButton(notify_text, callback_data='notify')
-    
+
     # Sends user back to the main menu.
     back_button = InlineKeyboardButton('⬅️ Назад в меню', callback_data='main_menu')
 
     keyboard = [[refresh_button],
                 [notify_button],
                   [back_button]]
-                  
+
     return(InlineKeyboardMarkup(keyboard))
 
 
-# Keyboard for notification. Only one button to delete message.
+# Keyboard for a notification. Only one button to delete message.
 def notify_keyboard():
-    
+
     del_notification_button = InlineKeyboardButton('Скрыть уведомление',  callback_data='delete_notification')
-    
+
     keyboard = [[del_notification_button]]
-    
+
     return(InlineKeyboardMarkup(keyboard))
