40b72ed9c1f869ccd60193fd7465ccdba645fd64
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-09-05 19:18:08 +0300

    Update send_message_interval from 3s to 0.05s

--- src/static.py
+++ src/static.py
@@ -16,7 +16,7 @@ statisticsdb_path = db_dir + 'statistics.db'
 
 # Intervals for update checks and notifications(s).
 check_updates_interval = 120
-send_message_interval = 3
+send_message_interval = 0.05
 
 # Class for storing timetable's options.
 class TTBS():
