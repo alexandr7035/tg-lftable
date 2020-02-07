f90c23bec21aeab777fbc340414dc68800c106e1
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-02-07 14:07:16 +0300

    Add credit and exams info to src.static.py

--- src/static.py
+++ src/static.py
@@ -78,3 +78,54 @@ mag_c1.shortname = 'mag_c1'
 mag_c2.url = 'https://law.bsu.by/pub/2/Raspisanie_mag_2_kurs.xls'
 mag_c2.name = 'Магистратура, 2-й курс'
 mag_c2.shortname = 'mag_c2'
+
+
+# This part is for credits and exams
+# The complication caused by division into summer and 
+# winter exam/credit timetable files on the site
+
+zachet_1k = TTBS()
+ekz_1k = TTBS()
+zachet_2k = TTBS()
+ekz_2k = TTBS()
+zachet_3k = TTBS()
+ekz_3k = TTBS()
+zachet_4k = TTBS()
+ekz_4k = TTBS()
+
+all_credit_exam_timetables = [zachet_1k, ekz_1k, zachet_2k, ekz_2k,
+                              zachet_3k, ekz_3k, zachet_4k, ekz_4k]
+
+zachet_1k.name = 'Зачеты, 1-й курс'
+zachet_1k.urls = {'winter' : 'https://law.bsu.by/pub/2/zima_zachet_1k.xls',
+                  'summer' : 'https://law.bsu.by/pub/2/leto_zachet_1k.xls'}
+
+ekz_1k.name = 'Экзамены, 1-й курс'
+ekz_1k.urls = {'winter' : 'https://law.bsu.by/pub/2/zima_ekz_1k.xls',
+                  'summer' : 'https://law.bsu.by/pub/2/leto_ekz_1k.xls'}
+                
+
+zachet_2k.name = 'Зачеты, 2-й курс'
+zachet_2k.urls = {'winter' : 'https://law.bsu.by/pub/2/zima_zachet_2k.xls',
+                  'summer' : 'https://law.bsu.by/pub/2/leto_zachet_2k.xls'}
+
+ekz_2k.name = 'Экзамены, 2-й курс'
+ekz_2k.urls = {'winter' : 'https://law.bsu.by/pub/2/zima_ekz_2k.xls',
+                  'summer' : 'https://law.bsu.by/pub/2/leto_ekz_2k.xls'}
+
+
+zachet_3k.name = 'Зачеты, 3-й курс'
+zachet_3k.urls = {'winter' : 'https://law.bsu.by/pub/2/zima_zachet_3k.xls',
+                  'summer' : 'https://law.bsu.by/pub/2/leto_zachet_3k.xls'}
+
+ekz_3k.name = 'Экзамены, 3-й курс'
+ekz_3k.urls = {'winter' : 'https://law.bsu.by/pub/2/zima_ekz_3k.xls',
+                  'summer' : 'https://law.bsu.by/pub/2/leto_ekz_3k.xls'}
+    
+
+# Only winter timetable avaliable for course 4
+zachet_4k.name = 'Зачеты, 4-й курс'
+zachet_4k.urls = {'winter' : 'https://law.bsu.by/pub/2/zima_zachet_4k.xls'}
+
+ekz_4k.name = 'Экзамены, 4-й курс'
+ekz_4k.urls = {'winter' : 'https://law.bsu.by/pub/2/zima_ekz_4k.xls'}
